f955d840cebbe2b2e185f0192c9e0154177c94c8
Ji SeungMin <jiprone1@gmail.com>
2024-07-28 16:01:10 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ MySQL/프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -0,0 +1,11 @@
+-- 코드를 작성해주세요
+select distinct item_id, item_name, rarity 
+from item_info
+where item_id in(
+    select item_id
+    from item_tree
+    where parent_item_id in(
+        select item_id
+        from item_info
+        where rarity = 'RARE'))
+order by item_id desc;
